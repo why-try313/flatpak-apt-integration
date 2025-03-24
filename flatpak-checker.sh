b12683b72a6925cb6655ccf143d5bf773e7485d6
WhyTry313 <18019207+why-try313@users.noreply.github.com>
2025-03-24 22:29:28 +0100

    Create flatpak-checker.sh

---
+++ flatpak-checker.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+
+# Hook installation:
+# file: /etc/apt/apt.conf.d/05flatpak-hook 
+# contents: APT::Update::Post-Invoke {"/my/path/to/this/file/flatpak-checker.sh"};
+
+echo "Checking Flatpak updates availible..."
+USR="$USER" # Switch  back to curent user instead of sudo:root
+if [ $USER = "root" ]; then USR="$SUDO_USER"; fi
+
+# Get updates
+UPDATES=$(sudo -u "$USR" 'n\n' 2>/dev/null | flatpak update | grep -Eo "^[\ ]*[0-9]+\..*" --color=none)
+UPDATES_NB=$(echo "$UPDATES" | tr -d '\n' | wc -l)
+
+if [ $UPDATES_NB = "0" ]; then
+	echo "Flatpak: No Updates found, skipping..."
+	exit
+fi
+
+echo "[i] $UPDATES_NB FlatPak updates found"
+echo "$UPDATES"
+echo '[i] To udpdate, make sure you run "flatpak update"'
